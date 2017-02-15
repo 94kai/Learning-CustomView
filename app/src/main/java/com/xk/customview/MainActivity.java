7911c89919dc89880e5f6b505c223b48855c6517
薛凯 <3440395@qq.com>
2017-02-15 17:32:41 +0800

    。。

--- app/src/main/java/com/xk/customview/MainActivity.java
+++ app/src/main/java/com/xk/customview/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.xk.customview.custom.MCustom;
+import com.xk.customview.custom.PieData;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void requestLayout(View c){
-        findViewById(R.id.mc).requestLayout();
+        MCustom viewById = (MCustom) findViewById(R.id.mc);
+        PieData a1 = new PieData("x1x", 10);
+        PieData a2 = new PieData("x2x", 20);
+        PieData a3 = new PieData("x3x", 130);
+        PieData a4 = new PieData("x4x", 40);
+        ArrayList<PieData> pieDatas = new ArrayList<>();
+        pieDatas.add(a1);
+        pieDatas.add(a2);
+        pieDatas.add(a3);
+        pieDatas.add(a4);
+        viewById.setData(pieDatas);
     }
 }
--- app/src/main/java/com/xk/customview/custom/MCustom.java
+++ app/src/main/java/com/xk/customview/custom/MCustom.java
@@ -3,48 +3,112 @@ package com.xk.customview.custom;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
+
+import java.util.ArrayList;
+
+import static android.R.attr.start;
 
 /**
  * Created by xuekai on 2017/2/14.
  */
 
 public class MCustom extends View {
-    private static final String TAG = "MCustom";
-    public MCustom(Context context) {
-        super(context);
-    }
+        // 颜色表(注意: 此处定义颜色使用的是ARGB，带Alpha通道的)
+        private int[] mColors = {0xFFCCFF00, 0xFF6495ED, 0xFFE32636, 0xFF800000, 0xFF808000, 0xFFFF8C69, 0xFF808080,
+                0xFFE6B800, 0xFF7CFC00};
+        // 饼状图初始绘制角度
+        private float mStartAngle = 0;
+        // 数据
+        private ArrayList<PieData> mData;
+        // 宽高
+        private int mWidth, mHeight;
+        // 画笔
+        private Paint mPaint = new Paint();
 
-    public MCustom(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+        public MCustom(Context context) {
+            this(context, null);
+        }
 
-    public MCustom(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+        public MCustom(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setAntiAlias(true);
+        }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            mWidth = w;
+            mHeight = h;
+        }
 
-        Log.e(TAG,"onMeasure");
-    }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (null == mData)
+                return;
+            float currentStartAngle = mStartAngle;                    // 当前起始角度
+//            canvas.translate(mWidth / 2, mHeight / 2);                // 将画布坐标原点移动到中心位置
+            float r = (float) (Math.min(mWidth, mHeight) / 2 * 0.8);  // 饼状图半径
+            RectF rect = new RectF(0, 0, 2*r, 2*r);                     // 饼状图绘制区域
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed,left,top,right,bottom);
-        Log.e(TAG, "onLayout");
-    }
+            for (int i = 0; i < mData.size(); i++) {
+                PieData pie = mData.get(i);
+                mPaint.setColor(pie.getColor());
+                canvas.drawArc(rect, currentStartAngle, pie.getAngle(), true, mPaint);
+                currentStartAngle += pie.getAngle();
+            }
 
+        }
 
+        // 设置起始角度
+        public void setStartAngle(int mStartAngle) {
+            this.mStartAngle = mStartAngle;
+            invalidate();   // 刷新
+        }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Log.e(TAG, "onDraw" );
-    }
+        // 设置数据
+        public void setData(ArrayList<PieData> mData) {
+            this.mData = mData;
+            initData(mData);
+            invalidate();   // 刷新
+        }
 
+        // 初始化数据
+        private void initData(ArrayList<PieData> mData) {
+            if (null == mData || mData.size() == 0)   // 数据有问题 直接返回
+                return;
+
+            float sumValue = 0;
+            for (int i = 0; i < mData.size(); i++) {
+                PieData pie = mData.get(i);
+
+                sumValue += pie.getValue();       //计算数值和
+
+                int j = i % mColors.length;       //设置颜色
+                pie.setColor(mColors[j]);
+            }
+
+            float sumAngle = 0;
+            for (int i = 0; i < mData.size(); i++) {
+                PieData pie = mData.get(i);
+
+                float percentage = pie.getValue() / sumValue;   // 百分比
+                float angle = percentage * 360;                 // 对应的角度
+
+                pie.setPercentage(percentage);                  // 记录百分比
+                pie.setAngle(angle);                            // 记录角度大小
+                sumAngle += angle;
+
+                Log.i("angle", "" + pie.getAngle());
+            }
+        }
 }
