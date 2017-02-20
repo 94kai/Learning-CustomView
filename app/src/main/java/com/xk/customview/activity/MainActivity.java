66e591b996222de2da5858cf60eabc0d17161183
薛凯 <3440395@qq.com>
2017-02-20 17:53:05 +0800

    demo

--- app/src/main/java/com/xk/customview/activity/MainActivity.java
+++ app/src/main/java/com/xk/customview/activity/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] datas = {"0.圆->心", "1.叶子进度条", "2.弹性球", "3.蜘蛛网（游戏属性图）", "4.qq未读消息气泡", "5.刻度表"};
+        String[] datas = {"0.Demo", "1.叶子进度条", "2.弹性球", "3.蜘蛛网（游戏属性图）", "4.qq未读消息气泡", "5.刻度表", "6.圆->心"};
 
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas));
@@ -31,27 +31,25 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        startActivity(new Intent(MainActivity.this, CircleAndHeartActivity.class));
+                        startActivity(new Intent(MainActivity.this, DemoActivity.class));
                         break;
                     case 1:
                         startActivity(new Intent(MainActivity.this, LeafProgressActivity.class));
-
                         break;
                     case 2:
                         startActivity(new Intent(MainActivity.this, MagicCircleActivity.class));
-
                         break;
                     case 3:
                         startActivity(new Intent(MainActivity.this, SpiderActivity.class));
-
-                        break;
                     case 4:
                         startActivity(new Intent(MainActivity.this, QQBubbleActivity.class));
-
                         break;
                     case 5:
                         startActivity(new Intent(MainActivity.this, InstrumentActivity.class));
+                        break;
 
+                    case 6:
+                        startActivity(new Intent(MainActivity.this, CircleAndHeartActivity.class));
                         break;
                 }
             }
